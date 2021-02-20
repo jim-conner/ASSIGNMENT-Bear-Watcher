ce3e9c9aff4fd23ecbc7f4c70954fef66896cc7a
Jim Administrator <jamesdavidconner@gamil.com>
2021-02-20 09:02:40 -0600

    added two new functions but haven't compiled yet

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -6,33 +6,66 @@ const printToDom = (divId, textToPrint) => {
   selectedDiv.innerHTML = textToPrint;
 };
 
-const getBearForm = () => {
+const bears = [];
+
+const printBearForm = () => {
   const domString = `<form>
   <h5 class="bear-name"></h5>
   <div class="form-group">
     <label for="formGroupExampleInput">Bear Name</label>
-    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Add bear name">
+    <input type="text" class="form-control" id="name" placeholder="Add bear name">
   </div>
   <div class="form-group">
     <label for="formGroupExampleInput2">Bear Image URL</label>
-    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Add a bear image">
+    <input type="text" class="form-control" id="imageUrl" placeholder="Add a bear image">
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" id="submit" class="btn btn-primary">Submit</button>
   </form>`;
 
-  printToDom('#form', domString);
+  printToDom('#bear-form', domString);
 };
 
-// const addBear = (taco) {
-//   domstring = '';
-//   taco.forEach((card, i) => {
-//   });
-// };
+const addBear = (taco) {
+  domstring = '';
+  taco.forEach((card, i) => {
+    `<div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="('${card.imageUrl}')" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${card.name}</h5>
+          <p class="card-text">Some quick example text to build on the card title & content.</p>
+          <button type="button" id="addBear" class="btn btn-primary" id="${i}">Add Bear</button>
+        </div>
+    </div>`
+  });
+};
+
+const getBearInfo = (e) => {
+  e.preventDefault();
+  
+  const name = document.querySelector("#name").value;
+  const image = document.querySelector("#imageUrl").value;
+
+  const obj = {
+    name,
+    imageUrl,
+  };
+
+  bears.push(obj);
+
+  addBear(bears);
+
+  document.querySelector("#form").reset();
+}; 
+
+buttonEvents = () => {
+  document.querySelector("#submit").addEventListener("click", printBearForm);
+};
 
 const init = () => {
-  getBearForm();
+  printBearForm();
+  addBear();
+  getBearInfo();
+
 };
 
 init();
-
-// export default printToDom;
