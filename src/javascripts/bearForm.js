ddcc0c045380357dbf6f72bc06d5db6625b4e9f8
Jim Administrator <jamesdavidconner@gamil.com>
2021-02-23 15:47:49 -0600

    redesigned flex box, added background image

--- src/javascripts/bearForm.js
+++ src/javascripts/bearForm.js
@@ -4,11 +4,9 @@ const printBearForm = () => {
   const domString = `<form>
   <h5 class="bear-name"></h5>
   <div class="form-group">
-    <label for="formGroupExampleInput">Bear Name</label>
     <input type="text" class="form-control" id="name" placeholder="Add bear name">
   </div>
   <div class="form-group">
-    <label for="imageUrl">Bear Image</label>
     <input type="url" class="form-control" id="image" placeholder="Add a bear image URL">
   </div>
   <button type="submit" id="add" class="btn btn-primary">Add Bear</button>
